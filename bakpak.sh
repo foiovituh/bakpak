b0b27914d8ad025e8228b0371eac01336bfc8c1d
foiovituh <foiovituh@outlook.com>
2025-06-29 10:04:07 -0300

    Implement argument parsing with getopts

--- bakpak.sh
+++ bakpak.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
-readonly ERRORS_LOG_FILE_PATH='/tmp/bakpak_errors.log';
-readonly EXECUTION_PATH="$(dirname "$0")"
-readonly SCRIPT_VERSION="1.0.0"
+declare -r LOG_ERROR_PATH='/tmp/bakpak_errors.log';
+declare -r EXECUTION_PATH="$(dirname "$0")"
+declare -r VERSION='1.0.0'
+declare -r TAR_GZ='tar.gz'
+declare -r TAR='tar'
 
 timestamp() {
     date +"%Y-%m-%dT%H:%M:%S"
@@ -11,66 +13,71 @@ log() {
     echo -e "$(timestamp) | $1"
 }
 
-show_version() {
-    echo "bakpak version: $SCRIPT_VERSION"
-    exit 0
+warn() {
+    log "[WARN] $1"
+    echo -e "\n---\n"
+    show_help 1
 }
 
 show_help() {
     cat <<EOF
-Usage: bakpak <from> <to>
+Basic usage:   bakpak <from> <to>
 
-from: Directory to back up (must be readable)
-to:   Directory to store the compressed archive (must be writable)
+Required:
+  -f <path>    Directory to back up (must be readable)
+  -t <path>    Directory to store the compressed archive (must be writable)
 
-Compresses the from directory and stores the archive in the to directory.
-Errors are logged to /tmp/bakpak_errors.log
+Optional:
+  -v           Show script version
+  -h           Show this help message
+  -p           Custom backup name prefix
+  -u           Create uncompressed .tar archive
 
-Options:
-  -v, --version     Show script version
-  -h, --help        Show this help message
+Examples:
+  bakpak -f ~/Documents -t /mnt/backups
+  bakpak -f ~/Documents -t /mnt/backups -p docs-backup -u
 
 Author:
-  Vituh             <foiovituh@outlook.com>
-  GitHub:           https://github.com/foiovituh
+  Vituh        <foiovituh@outlook.com>
+  GitHub:      https://github.com/foiovituh
 EOF
     exit "$1"
 }
 
 create_backup() {
-    local from="$1"
-    local to="$2"
-
     if [[ ! -d "$from" || ! -r "$from" || ! -d "$to" || ! -w "$to" ]]; then
-        show_help 1
+        warn "-f must be readable and -t must be writable directories"
     fi
 
-    if [[ "$to" != */ ]]; then
-        to="${to}/"
-    fi
-
+    local extension="$([[ "$no_compress" == true ]] \
+        && echo "$TAR" \
+        || echo "$TAR_GZ")"
+    
     local from_basename="$(basename "$from")"
+    local file_prefix="${prefix:-$from_basename}"
 
-    if ! tar -czf "${to}${from_basename}_$(date +"%F_%H-%M-%S").tar.gz" \
+    if ! tar -czf "${to}${file_prefix}_$(date +"%F_%H-%M-%S").${extension}" \
         -C "$(dirname "$from")" "$from_basename" \
-        2> "$ERRORS_LOG_FILE_PATH"; then
-        log "Backup failed! See ${ERRORS_LOG_FILE_PATH} for details."
-        exit 1
+        2> "$LOG_ERROR_PATH"; then
+        warn "Backup failed! See ${LOG_ERROR_PATH} for details."
     fi
 
     log "Backup created at: ${to}"
 }
 
-readonly FIRST_ARGUMENT="$1"
-readonly SECOND_ARGUMENT="$2"
+uncompressed=false
 
-case "$FIRST_ARGUMENT" in
-    -v|--version)
-        show_version
-        ;;
-    -h|--help)
-        show_help 0
-        ;;
-esac
+while getopts ":f:t:p:uvh" opt; do
+  case "$opt" in
+    f) declare -r from="$OPTARG" ;;
+    t) to="${OPTARG%/}/" ;;
+    p) declare -r prefix="$OPTARG" ;;
+    u) uncompressed=true ;;
+    v) echo "bakpak version ${VERSION}"; exit 0 ;;
+    h) show_help 0;;
+    \?) warn "Invalid option: -${OPTARG}";;
+    :) warn "The -${OPTARG} option requires argument";;
+  esac
+done
 
-create_backup "$FIRST_ARGUMENT" "$SECOND_ARGUMENT"
+create_backup
